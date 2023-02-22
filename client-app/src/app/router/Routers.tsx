50bfc09688ee6c5bf4fb6f211bd6c744317f2593
trungquan21 <89087554+trungquan21@users.noreply.github.com>
2023-02-22 21:47:02 +0700

    #updateUIResetpassword

--- client-app/src/app/router/Routers.tsx
+++ client-app/src/app/router/Routers.tsx
@@ -6,6 +6,7 @@ import RequireAuth from "./RequireAuth";
 import Test from "../../features/Test/Test";
 import PasswordReset from "../../features/Account/PasswordReset";
 import PasswordResetToken from "../../features/Account/PasswordResetToken";
+import PasswordResetSuccess from "../../features/Account/PasswordResetSuccess";
 import RequireNonAuth from "./RequireNonAuth";
 import Login from "../../features/Account/Login";
 import HomePage from "../../features/Hompage/HomePage";
@@ -15,6 +16,7 @@ export const route = {
     login: '/login',
     resetPassword: '/account/password_reset',
     confirmResetPassword: '/account/password_reset/with',
+    resetPasswordSuccess: '/account/password_reset_success/with',
     homepage: '/homepage',
 };
 
@@ -32,6 +34,7 @@ export const routes: RouteObject[] = [
                     {path: 'login', element: <Login/>},
                     {path: 'account/password_reset', element: <PasswordReset/>},
                     {path: 'account/password_reset/with', element: <PasswordResetToken/>},
+                    {path: 'account/password_reset_success/with', element: <PasswordResetSuccess/>},
                 ]
             },
             {path: 'not-found', element: <NotFound/>},
