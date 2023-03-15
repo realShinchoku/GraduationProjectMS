07604fb2e34e9ff4dfb5273bf92b20f5ef08e4da
Shinchoku <tienmp3@gmail.com>
2023-03-15 23:21:51 +0700

    Update Test.tsx

--- client-app/src/features/Test/Test.tsx
+++ client-app/src/features/Test/Test.tsx
@@ -2,8 +2,6 @@ import {observer} from "mobx-react-lite";
 import TestApi from "./Api/TestApi";
 import {useStore} from "../../app/stores/store";
 import {Button} from "@mui/material";
-import { SinhVien } from "../../app/models/sinhVien";
-import { useEffect } from "react";
 
 function Test() {
     const {modalStore} = useStore();
