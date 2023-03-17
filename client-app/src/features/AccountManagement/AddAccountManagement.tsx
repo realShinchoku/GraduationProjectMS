2ad5829c3419959121216cf230efbdeb581626ed
phudo1938 <104451571+phudo1938@users.noreply.github.com>
2023-03-17 14:28:55 +0700

    update_addAccountManagement

--- client-app/src/features/AccountManagement/AddAccountManagement.tsx
+++ client-app/src/features/AccountManagement/AddAccountManagement.tsx
@@ -1,16 +1,15 @@
-import {Box, Button, ButtonGroup, Grid, Typography} from '@mui/material';
+import { Box, Button, ButtonGroup, Grid, Typography } from '@mui/material';
 import UploadFileIcon from '@mui/icons-material/UploadFile';
-import {useDropzone} from 'react-dropzone';
-import {useStore} from "../../app/stores/store";
-import {useState} from "react";
+import { useDropzone } from 'react-dropzone';
+import { useStore } from "../../app/stores/store";
+import { useState } from "react";
 import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined';
-import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
 import "./AccountManagement.scss"
-
+import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
 export default function AddAccountManagement() {
 
     const [file, setFile] = useState<any>();
-    const {getRootProps, getInputProps, acceptedFiles, isDragActive, isDragAccept, isDragReject} = useDropzone({
+    const { getRootProps, getInputProps, acceptedFiles, isDragActive, isDragAccept, isDragReject } = useDropzone({
         accept: {
             'text/csv': ['.csv',]
         },
@@ -21,7 +20,7 @@ export default function AddAccountManagement() {
         },
     });
 
-    const {modalStore} = useStore();
+    const { modalStore } = useStore();
     return (
         <Grid className='add_account'>
             <Grid className="modal_body">
@@ -33,10 +32,10 @@ export default function AddAccountManagement() {
                 <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={1}>
                     <Box className="select_user">
                         <Box marginTop={'50%'} gridColumn="span 2">
-                            <Box className="select_user_" sx={{display: 'flex', '& > *': {m: 1,}}}>
-                                <ButtonGroup sx={{margin: 'auto', borderRadius: 'unset'}} fullWidth
-                                             orientation="vertical" aria-label="vertical outlined button group"
-                                             variant="outlined">
+                            <Box className="select_user_" sx={{ display: 'flex', '& > *': { m: 1, } }}>
+                                <ButtonGroup sx={{ margin: 'auto', borderRadius: 'unset' }} fullWidth
+                                    orientation="vertical" aria-label="vertical outlined button group"
+                                    variant="outlined">
                                     <Button className="button_add_account" key="one">Sinh Viên</Button>
                                     <Button className="button_add_account" key="two">Giảng Viên</Button>
                                     <Button className="button_add_account" key="three">Bộ Môn</Button>
@@ -44,7 +43,7 @@ export default function AddAccountManagement() {
                             </Box>
                         </Box>
                     </Box>
-                    <Box sx={{height: 'fit-content',}} gridColumn="span 11">
+                    <Box sx={{ height: 'fit-content', }} gridColumn="span 11">
                         <Box className="top_dropzone">
                             <Typography className="title_top_dropzone" variant="h6" color="inherit" component="div">
                                 Tải File lên
@@ -52,23 +51,27 @@ export default function AddAccountManagement() {
                             <Button className="button_up_file" variant="contained" onClick={() => console.log(file)}>Tải
                                 lên</Button>
                         </Box>
-                        <Box {...getRootProps({className: 'container_dropzone'})}>
-                            <UploadFileIcon className="icon_dropzone"/>
+                        <Box {...getRootProps({ className: 'container_dropzone' })}>
+                            {!(isDragActive || acceptedFiles.length > 0) && <UploadFileIcon className='icon_dropzone_' />}
                             <input {...getInputProps()} />
-                            {file && <Box className='dropzone_file_csv'>{/* preview icon css lai cho giong ben kia*/}
+                            {!(isDragActive || acceptedFiles.length > 0) && <Box className="icon_dropzone">
+                                <Typography className='typo_dropzone'>Kéo thả file vào đây để nhập thông tin. Định dạng hỗ trợ .csv</Typography>
+                            </Box>}
+                            {file && <Box className='dropzone_file_csv'>{}
+                                <Button className={"icon_delete"} onClick={() => {
+                                    acceptedFiles.slice(0);
+                                    setFile(undefined);
+                                }}
+                                    sx={{ padding: '0px 0px 2px 50px', transform: 'translateX(8px)', position: 'absolute', bottom: '0', right: '0', border: 'none', ':hover': { border: 'none', background: 'none' } }} variant="outlined" startIcon={<DeleteForeverOutlinedIcon sx={{ '&:nth-of-type(1)': { fontSize: '15px' }, color: '#bebebe' }} />}
+                                />
                                 <DescriptionOutlinedIcon className='icon_dropfile'></DescriptionOutlinedIcon>
-                            <div className={"file_name"}>{file.name}</div>
-                            <Button className={"icon_delete"} onClick={() => {
-                                acceptedFiles.slice(0);
-                                setFile(undefined);
-                            }}
-                            />
-                        </Box>}
+                                <div className={"file_name"}>{file.name}</div>
+                            </Box>}
                         </Box>
                     </Box>
                     <Box className="button_exit">
                         <Button className="button_exit_dropzone" variant="contained"
-                                onClick={() => modalStore.closeModal()}>Thoát</Button>
+                            onClick={() => modalStore.closeModal()}>Thoát</Button>
                     </Box>
                 </Box>
             </Grid>
