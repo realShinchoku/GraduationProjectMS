47475f7aef33361d95a2621e85d3f81399eb3915
Shinchoku <tienmp3@gmail.com>
2023-03-21 22:14:07 +0700

    Update LecturerTableRowDetail.tsx

--- client-app/src/features/Lecturers/List/LecturerTableRowDetail.tsx
+++ client-app/src/features/Lecturers/List/LecturerTableRowDetail.tsx
@@ -87,7 +87,7 @@ function LecturerTableRowDetail({value, lecturer}: Props) {
                                     <Box component="img" src={LecturerImages.ContactLecturer} alt="" />
                                     <Typography variant="body1">Liên hệ hướng dẫn</Typography>
                                 </Button>
-                                <Button className="choose_lecturer" onClick={chose(lecturer.id).then(() =>setIsActive(current => !current))}>Chọn giảng viên</Button>
+                                <Button className="choose_lecturer" onClick={() => chose(lecturer.id).then(() =>setIsActive(current => !current))}>Chọn giảng viên</Button>
                             </ButtonGroup>
                         </Grid>
                     </Grid>
