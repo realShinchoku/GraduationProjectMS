37a2ff46a482aad0db7af86620747c15d2662a1d
Shinchoku <tienmp3@gmail.com>
2023-04-08 06:06:45 +0700

    c

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -66,7 +66,7 @@ public static class ApplicationServiceExtensions
                 var pgHost = pgHostPort.Split(":")[0];
                 var pgPort = pgHostPort.Split(":")[1];
 
-                connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
+                connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL=false;SslMode=Disable;";
             }
 
             // Whether the connection string came from the local development configuration file
